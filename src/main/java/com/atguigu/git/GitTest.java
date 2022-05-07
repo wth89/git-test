836f56b6fbe3e0231229a857411718cf552c69d4
wth <2895416935@qq.com>
2022-05-07 17:33:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wth
+ * @create 2022/5/7 - 17:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
