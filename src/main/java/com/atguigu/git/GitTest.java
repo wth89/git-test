bc390b58962702c1e4401bb51f58fd4fef79e1f3
wth <2895416935@qq.com>
2022-05-07 22:06:01 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("pull test");
+        System.out.println("gitee test");
+
     }
 }
