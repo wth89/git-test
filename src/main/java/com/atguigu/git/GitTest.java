175a45d2065281dedfe16de29c0b95613e5122f5
wth <2895416935@qq.com>
2022-05-07 17:39:40 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,10 @@ package com.atguigu.git;
 
 /**
  * @author wth
- * @create 2022/5/7 - 17:25
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
     }
